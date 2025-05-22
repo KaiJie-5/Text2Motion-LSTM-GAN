a0d2170afe3aeb12aaf5be1601c7a0f12dc9a906
KaiJie-5 <liangkj75@gmail.com>
2025-05-22 09:26:27 +0100

    Include hyperparameter tuning configurations and ablation study experiments

---
+++ Hyperparameter_Tuning_and_Ablation_Study/Model/Case_3/execute_model.py
@@ -0,0 +1,165 @@
+from structure_GAN import create_Generator, create_discriminator, train_d, train_g
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import os
+import scipy.io as scio
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+# Load your data
+train_script = np.load('/mainfs/lyceum/kjl1a21/Filestone/Individual_Project/2nd_week_3rd_week/LSTM_GAN/train_test_data/train_script.npy')  #(25535, 512)
+train_action = np.load('/mainfs/lyceum/kjl1a21/Filestone/Individual_Project/2nd_week_3rd_week/LSTM_GAN/train_test_data/train_action.npy')  #(25535, 32, 24)
+val_action = np.load('/mainfs/lyceum/kjl1a21/Filestone/Individual_Project/2nd_week_3rd_week/LSTM_GAN/train_test_data/val_action.npy') #(1595, 32, 24)
+val_script = np.load('/mainfs/lyceum/kjl1a21/Filestone/Individual_Project/2nd_week_3rd_week/LSTM_GAN/train_test_data/val_script.npy') #(1595, 512)
+init_pose = scio.loadmat('/lyceum/kjl1a21/Filestone/CoSpeech-T2A_Supervisor/data/mean_pose.mat')['mean_vector']
+init_pose = np.transpose(init_pose,(1,0))
+
+print(f'Train Action Min :{train_action.min()}')
+print(f'Train Action Max :{train_action.max()}')
+print(f'Val Action Min :{val_action.min()}')
+print(f'Val Action Max :{val_action.max()}')
+print(f'Train Script Min :{train_script.min()}')
+print(f'Train Script Max :{train_script.max()}')
+print(f'Val Script Min :{val_script.min()}')
+print(f'Val Script Max :{val_script.max()}')
+
+print(train_script.shape)
+print(train_action.shape)
+print(val_action.shape)
+print(val_script.shape)
+print(init_pose.shape)
+
+# Global variables
+latent_dim = 20
+text_dim = 512
+action_time_steps = 32
+dim_action = 24
+epochs = 150
+batch_size = 32 
+alpha = 1
+beta = 10
+gamma = 5
+case_number = 3
+
+import tensorflow as tf
+
+# Create a TensorFlow Dataset from training data
+dataset = tf.data.Dataset.from_tensor_slices((train_script, train_action))
+dataset = dataset.shuffle(buffer_size=1024).batch(batch_size, drop_remainder=True).prefetch(tf.data.AUTOTUNE)
+
+# Create a TensorFlow Dataset from validation data
+val_dataset = tf.data.Dataset.from_tensor_slices((val_script, val_action))
+val_dataset = val_dataset.batch(batch_size, drop_remainder=True).prefetch(tf.data.AUTOTUNE)
+
+batched_input = tf.tile(tf.expand_dims(init_pose, axis=0), [batch_size, 1, 1])
+print(batched_input.shape)
+
+model_saved_path = f'/mainfs/lyceum/kjl1a21/Filestone/Individual_Project/2nd_week_3rd_week/LSTM_GAN/Test_GAN/Generated_Action_2nd_prototype/Model_and_Results/Case_{case_number}/'
+generated_output_saved_path = f'/mainfs/lyceum/kjl1a21/Filestone/Individual_Project/2nd_week_3rd_week/LSTM_GAN/Test_GAN/Generated_Action_2nd_prototype/Test_and_Generated_Data/Case_{case_number}/Generated_Data/'
+test_action_saved_path = f'/mainfs/lyceum/kjl1a21/Filestone/Individual_Project/2nd_week_3rd_week/LSTM_GAN/Test_GAN/Generated_Action_2nd_prototype/Test_and_Generated_Data/Case_{case_number}/Test_Data/'
+
+# List of paths to check and create
+paths = [
+    model_saved_path,
+    generated_output_saved_path,
+    test_action_saved_path
+]
+
+for path in paths:
+    if not os.path.exists(path):
+        os.makedirs(path, exist_ok = True)
+
+# Create models
+generator = create_Generator(latent_dim, text_dim, action_time_steps,init_pose)
+discriminator = create_discriminator((action_time_steps, dim_action), text_dim)
+
+print(generator.summary())
+print(discriminator.summary())
+
+# Define optimizers
+generator_optimizer = tf.keras.optimizers.Adam(learning_rate= 0.00002)
+discriminator_optimizer = tf.keras.optimizers.Adam(learning_rate=0.00002)
+
+# Define loss function  
+cross_entropy = tf.keras.losses.BinaryCrossentropy()
+mean_squared_error = tf.keras.losses.MeanSquaredError()
+
+epoch_d_loss = []
+epoch_g_loss = []
+epoch_g_distance_loss = []
+epoch_acceleration_loss = []
+epoch_real_loss = []
+epoch_fake_loss = []
+
+d_loss_array = []
+g_loss_array = []
+real_loss_array = []
+fake_loss_array = []
+g_distance_loss_array = []
+validation_loss_array = []
+acceleration_loss_array = []
+
+# Training loop
+for epoch in range(epochs + 1):
+
+    all_val_generated = []
+    all_val_real = []
+
+    for train_text_batch, train_action_batch in dataset:
+
+        noise = tf.random.normal([batch_size, latent_dim])
+
+        discriminator_loss, r_loss, f_loss = train_d(generator, discriminator, noise, train_text_batch, train_action_batch, cross_entropy, discriminator_optimizer,batched_input)
+        generator_loss, generator_distance_loss, acceleration_loss = train_g(generator, discriminator, noise, train_text_batch, train_action_batch, cross_entropy, mean_squared_error, generator_optimizer, alpha, beta, gamma, batched_input)
+
+        epoch_d_loss.append(discriminator_loss)
+        epoch_g_loss.append(generator_loss)
+        epoch_g_distance_loss.append(generator_distance_loss)
+        epoch_acceleration_loss.append(acceleration_loss)
+        epoch_real_loss.append(r_loss)
+        epoch_fake_loss.append(f_loss)
+    
+    d_loss_array.append(np.mean(epoch_d_loss))
+    g_loss_array.append(np.mean(epoch_g_loss))
+    g_distance_loss_array.append(np.mean(epoch_g_distance_loss))
+    acceleration_loss_array.append(np.mean(epoch_acceleration_loss))
+    real_loss_array.append(np.mean(epoch_real_loss))
+    fake_loss_array.append(np.mean(epoch_fake_loss))
+
+    for val_text_batch, val_action_batch in val_dataset:
+
+        current_batch_size = val_text_batch.shape[0]
+        noise = tf.random.normal([current_batch_size, latent_dim])
+
+        generated_output = generator([noise,val_text_batch,batched_input], training=False)
+
+        #Save the test and generated action batch for evaluation
+
+        all_val_generated.append(generated_output.numpy())
+        all_val_real.append(val_action_batch.numpy())
+
+    # Save the model every 50 epochs
+    if epoch % 50 == 0:
+        generator.save(f"/mainfs/lyceum/kjl1a21/Filestone/Individual_Project/2nd_week_3rd_week/LSTM_GAN/Test_GAN/Generated_Action_2nd_prototype/Model_and_Results/Case_{case_number}/model_epoch_{epoch}.keras")
+
+        # Save to files
+        np.save(f'/mainfs/lyceum/kjl1a21/Filestone/Individual_Project/2nd_week_3rd_week/LSTM_GAN/Test_GAN/Generated_Action_2nd_prototype/Test_and_Generated_Data/Case_{case_number}/Generated_Data/generated_output_epoch_{epoch}.npy', np.concatenate(all_val_generated))
+        np.save(f'/mainfs/lyceum/kjl1a21/Filestone/Individual_Project/2nd_week_3rd_week/LSTM_GAN/Test_GAN/Generated_Action_2nd_prototype/Test_and_Generated_Data/Case_{case_number}/Test_Data/test_action_batch_epoch_{epoch}.npy', np.concatenate(all_val_real))
+
+    print(f"{epoch}/{epochs} [Real loss: {r_loss}] [Fake loss: {f_loss}] [G loss: {generator_loss}] [G_distance_loss: {generator_distance_loss}] [Velocity Loss: {acceleration_loss}]")
+
+# Visualize losses
+plt.figure(figsize=(12, 6))
+plt.plot(range(epochs + 1), d_loss_array, label='Discriminator Loss')
+plt.plot(range(epochs + 1), g_loss_array, label='Generator Cross Entropy Loss')
+plt.plot(range(epochs + 1), g_distance_loss_array, label='Generator Distance Loss')
+plt.plot(range(epochs + 1), real_loss_array, label='Real Loss')
+plt.plot(range(epochs + 1), fake_loss_array, label='Fake Loss')
+plt.plot(range(epochs + 1), acceleration_loss_array, label='Velocity Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Training Loss')
+plt.title('LSTM-GAN Training Losses')
+plt.legend()
+plt.savefig(f"/mainfs/lyceum/kjl1a21/Filestone/Individual_Project/2nd_week_3rd_week/LSTM_GAN/Test_GAN/Generated_Action_2nd_prototype/Model_and_Results/Case_{case_number}/lstm_gan_training_losses.png")
+#plt.show()
---
+++ Hyperparameter_Tuning_and_Ablation_Study/Model/Case_9/structure_GAN.py
@@ -0,0 +1,143 @@
+import tensorflow as tf
+from tensorflow.keras import Input, Model
+from tensorflow.keras.layers import (Layer,LSTM, Dense, RepeatVector,Dropout,
+                                      Concatenate,Reshape, LayerNormalization, ReLU,
+                                      GlobalAveragePooling1D, Conv1D,SpectralNormalization)
+from calk_jerk_or_acceleration import tf_compute_velocity
+
+class StackLayer(Layer):
+    def call(self, inputs):
+        return tf.stack(inputs, axis=1)
+    
+class InitialStateLayer(Layer):
+    def __init__(self, units, **kwargs):
+        super().__init__(**kwargs)
+        self.units = units
+
+    def call(self, inputs):
+        batch_size = tf.shape(inputs)[0]  
+        return tf.zeros((batch_size, self.units))
+
+# Define the generator model
+def create_Generator(latent_dim, text_dim, action_time_step, init_pose):
+    input_noise = Input(shape=(latent_dim,), name='noise')
+    input_text = Input(shape=(text_dim,), name='text')
+    init_pose = Input(shape=(1,24), name='pose')
+
+    #Reshape init_pose
+    init_pose_reshaped = Reshape((24,))(init_pose)
+
+    # Repeat inputs for all timesteps
+    noise_32 = RepeatVector(action_time_step)(input_noise)
+    text_32 = RepeatVector(action_time_step)(input_text)
+    init_pose_32 = RepeatVector(action_time_step)(init_pose_reshaped)
+
+    combined = Concatenate()([noise_32, text_32, init_pose_32])
+
+    # Two LSTM processing
+    x = LSTM(512, return_sequences=True)(combined)
+    x = LSTM(512, return_sequences=True)(x)
+
+    outputs = Dense(24, activation='tanh')(x)
+
+    model = Model([input_noise,input_text,init_pose], outputs, name='Generator')
+    return model
+
+def create_discriminator(generated_input_shape, text_dim):
+    # Motion Input (Time, Motion_Dim)
+    motion_input = Input(shape=generated_input_shape, name='generated_action')
+    
+    # Text Input
+    text_input = Input(shape=(text_dim,), name='text')
+
+    # Apply 1D Conv on Motion Only
+    motion_features = SpectralNormalization(Conv1D(32, kernel_size=5, strides=1, padding="same", activation=None))(motion_input)
+    motion_features = LayerNormalization()(motion_features)
+    motion_features = ReLU()(motion_features)
+
+    motion_features = SpectralNormalization(Conv1D(64, kernel_size=5, strides=2, padding="same", activation=None))(motion_features)
+    motion_features = LayerNormalization()(motion_features)
+    motion_features = ReLU()(motion_features)
+
+    motion_features = SpectralNormalization(Conv1D(128, kernel_size=5, strides=2, padding="same", activation=None))(motion_features)
+    motion_features = LayerNormalization()(motion_features)
+    motion_features = ReLU()(motion_features)
+
+    # Global Average Pooling to Reduce Temporal Dimension
+    motion_features = GlobalAveragePooling1D()(motion_features)
+
+    # Concatenate Motion and Text Features
+    combined_features = Concatenate()([motion_features, text_input])
+
+    # Fully Connected Layers
+    combined_features = Dense(128)(combined_features)
+    combined_features = LayerNormalization()(combined_features)
+    combined_features = ReLU()(combined_features)
+
+    combined_features = Dropout(0.2)(combined_features)
+
+    # Output Layer (Real/Fake Classification)
+    outputs = Dense(1, activation='sigmoid')(combined_features)
+
+    # Create Model
+    model = Model([motion_input, text_input], outputs, name='Discriminator')
+
+    return model
+
+# Train Discriminator
+def train_d(generator, discriminator, noise, text_embeddings_batch, real_action_batch, cross_entropy, discriminator_optimizer,batched_input):
+    with tf.GradientTape() as disc_tape:
+        # Generate fake actions
+        fake_action_batch = generator([noise, text_embeddings_batch, batched_input], training=False)
+
+        # Discriminator outputs
+        real_output = discriminator([real_action_batch, text_embeddings_batch], training=True)
+        fake_output = discriminator([fake_action_batch, text_embeddings_batch], training=True)
+
+        # Generate real labels with random values between 0.9 and 1.0
+        real_labels = tf.random.uniform(shape=tf.shape(real_output), minval=0.9, maxval=1.0)
+
+        # Generate fake labels with random values between 0.0 and 0.1
+        fake_labels = tf.random.uniform(shape=tf.shape(fake_output), minval=0.0, maxval=0.1)
+
+        # Discriminator loss
+        real_loss = cross_entropy(real_labels, real_output)
+        fake_loss = cross_entropy(fake_labels, fake_output)
+        d_loss = real_loss + fake_loss
+
+    # Compute gradients
+    gradients_of_discriminator = disc_tape.gradient(d_loss, discriminator.trainable_variables)
+
+    # Apply gradients
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+    return d_loss.numpy(), real_loss.numpy(), fake_loss.numpy()
+
+def train_g(generator, discriminator, noise, text_embeddings_batch, real_action_batch, cross_entropy, mean_squared_error, generator_optimizer, alpha, beta, gamma, batched_input):
+    with tf.GradientTape() as gen_tape:
+        # Generate fake actions
+        fake_action_batch = generator([noise, text_embeddings_batch, batched_input], training=True)
+
+        # Discriminator outputs
+        fake_output = discriminator([fake_action_batch, text_embeddings_batch], training=False)
+
+        real_labels = tf.ones_like(fake_output)
+
+        # Compute acceleration loss
+        real_vel = tf_compute_velocity(real_action_batch)
+        fake_vel = tf_compute_velocity(fake_action_batch)
+        vel_loss = tf.reduce_mean(tf.abs(fake_vel - real_vel))
+
+        # Generator loss
+        g_loss = cross_entropy(real_labels, fake_output)
+        g_distance_loss = mean_squared_error(real_action_batch,fake_action_batch)
+
+        total_g_loss = alpha*g_loss + beta*g_distance_loss + gamma*vel_loss
+
+    # Compute gradients
+    gradients_of_generator = gen_tape.gradient(total_g_loss, generator.trainable_variables)
+
+    # Apply gradients
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+
+    return g_loss.numpy(), g_distance_loss.numpy(), gamma*vel_loss.numpy()
